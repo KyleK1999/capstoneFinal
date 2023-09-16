8b3413a08d10ecbfe7dc77b68c734b8e9f1df26b
Kyle <kylekibbe99@gmail.com>
2023-09-16 01:06:31 -0700

    fixed some userprofile stuff

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import PcHardware from './PcHardware'; 
-import SaveBuildButton from './SaveBuildButton';
+import PcHardware from './PcHardware';
 
 const UserProfile = () => {
   const [savedBuilds, setSavedBuilds] = useState([]);
   const [toggleMoreInfo, setToggleMoreInfo] = useState({});
-  const [showMoreInfo, setShowMoreInfo] = useState({});
-
 
   useEffect(() => {
     const fetchSavedBuilds = async () => {
@@ -15,7 +12,6 @@ const UserProfile = () => {
         if (response.status === 200) {
           const data = await response.json();
           setSavedBuilds(data.savedBuilds);
-          //console.log('Current saved builds:', data.savedBuilds); 
         } else {
           console.error('Failed to fetch saved builds');
         }
@@ -27,12 +23,18 @@ const UserProfile = () => {
     fetchSavedBuilds();
   }, []);
 
-  
+  const toggleMoreInfoFunc = (index, type) => {
+    setToggleMoreInfo(prevState => ({
+      ...prevState,
+      [`${index}_${type}`]: !prevState[`${index}_${type}`],
+    }));
+  };
+
   const calculateTotalPrice = (components) => {
     let total = 0;
     for (const key in components) {
-      if (components[key] && components[key].price) {
-        total += components[key].price;
+      if (components[key] && components[key].Price) {
+        total += components[key].Price;
       }
     }
     return total.toFixed(2);
@@ -40,48 +42,40 @@ const UserProfile = () => {
 
   const deleteBuild = (buildId) => {
     if (!buildId) {
-      console.error('Build ID is undefined.');
+      console.error('Build ID is undefined');
       return;
     }
-  
+
     fetch(`/delete_build/${buildId}`, {
       method: 'DELETE',
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Network response was not ok.');
-    })
+    .then(res => res.json())
     .then(data => {
-      setSavedBuilds(savedBuilds.filter(build => build.id !== buildId));
+      // Remove the deleted build from state
+      setSavedBuilds(prevBuilds => prevBuilds.filter(build => build.id !== buildId));
     })
     .catch(error => {
-      console.error("Failed to delete build:", error);
+      console.error(`Error occurred: ${error}`);
     });
   };
-  
-  
-  
 
   return (
     <div>
       <h2>Your Saved Builds</h2>
       <ul>
         {savedBuilds.map((build, index) => {
-          console.log("Current build object:", build);
           return (
             <li key={index}>
               <h3>Build Type: {build.build_type?.type_name || 'N/A'}</h3>
               <p>Total Price: ${calculateTotalPrice(build.components) || 'N/A'}</p>
               <div>
-                <PcHardware index={index} name='CPU' type='cpu' partInfo={build.components.cpu} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />
-                <PcHardware index={index} name='GPU' type='gpu' partInfo={build.components.gpu} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />  
-                <PcHardware index={index} name='Memory' type='memory' partInfo={build.components.memory} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />
-                <PcHardware index={index} name='Motherboard' type='motherboard' partInfo={build.components.motherboard} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />
-                <PcHardware index={index} name='Storage' type='storage' partInfo={build.components.storage} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />                    
-                <PcHardware index={index} name='PSU' type='psu' partInfo={build.components.psu} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />       
-                <PcHardware index={index} name='Case' type='case' partInfo={build.components.case} toggleMoreInfo={toggleMoreInfo} showMoreInfo={showMoreInfo} />
+                <PcHardware index={index} name='CPU' type='cpu' partInfo={build.components.cpu} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />
+                <PcHardware index={index} name='GPU' type='gpu' partInfo={build.components.gpu} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />  
+                <PcHardware index={index} name='Memory' type='memory' partInfo={build.components.memory} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />
+                <PcHardware index={index} name='Motherboard' type='motherboard' partInfo={build.components.motherboard} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />
+                <PcHardware index={index} name='Storage' type='storage' partInfo={build.components.storage} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />                    
+                <PcHardware index={index} name='PSU' type='psu' partInfo={build.components.psu} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />       
+                <PcHardware index={index} name='Case' type='case' partInfo={build.components.case} toggleMoreInfo={toggleMoreInfoFunc} showMoreInfo={toggleMoreInfo} />
               </div>
               <button onClick={() => deleteBuild(build.id)}>Delete</button>  
             </li>
@@ -90,7 +84,6 @@ const UserProfile = () => {
       </ul>
     </div>
   );
-  
 };
 
 export default UserProfile;
